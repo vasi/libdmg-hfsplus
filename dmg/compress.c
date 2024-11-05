d8e361ccae2eea4e179d73be682b641bdc220d89
Dave Vasilevsky <dave@vasilevsky.ca>
2024-11-05 18:50:42 -0500

    Support optional LZMA decompression

--- dmg/compress.c
+++ dmg/compress.c
@@ -5,6 +5,21 @@
 #include <bzlib.h>
 #include "dmg/adc.h"
 
+#ifdef HAVE_LIBLZMA
+  #include <lzma.h>
+
+  static int decompressLZMA(unsigned char* inBuffer, size_t inSize, unsigned char* outBuffer, size_t outBufSize, size_t *decompSize) {
+      lzma_ret lret;
+      uint64_t memlimit = UINT64_MAX;
+      size_t inPos = 0;
+      *decompSize = 0;
+
+      lret = lzma_stream_buffer_decode(&memlimit, LZMA_FAIL_FAST, NULL,
+        inBuffer, &inPos, inSize, outBuffer, decompSize, outBufSize);
+      return lret != LZMA_OK;
+  }
+#endif
+
 int decompressRun(uint32_t type,
                   unsigned char* inBuffer, size_t inSize,
                   unsigned char* outBuffer, size_t outBufSize, size_t expectedSize)
@@ -21,6 +36,10 @@ int decompressRun(uint32_t type,
     unsigned int bz2DecompSize = outBufSize;
     ret = (BZ2_bzBuffToBuffDecompress(outBuffer, &bz2DecompSize, inBuffer, inSize, 0, 0) != BZ_OK);
     decompSize = bz2DecompSize;
+#ifdef HAVE_LIBLZMA
+  } else if (type == BLOCK_LZMA) {
+    ret = decompressLZMA(inBuffer, inSize, outBuffer, outBufSize, &decompSize);
+#endif
   } else {
     fprintf(stderr, "Unsupported block type: %#08x\n", type);
     return 1;
