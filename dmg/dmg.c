9916e640025031235f947b70016b164a1920fdbe
Dave Vasilevsky <dave@vasilevsky.ca>
2024-11-05 20:48:00 -0500

    Cleanup option handling
    It was a mess, with lots of awkward assumptions about what other options
    were found. Use getopt like a normal program.
    
    This will allow us to add more options without losing our minds.

--- dmg/dmg.c
+++ dmg/dmg.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 #include <dmg/dmg.h>
 #include <dmg/filevault.h>
@@ -32,60 +33,74 @@ int buildInOut(const char* source, const char* dest, AbstractFile** in, Abstract
 	return TRUE;
 }
 
+int usage(const char *name) {
+	printf("usage: %s [extract|build|build2048|res|iso|dmg|attribute] <in> <out> (-k <key>) (partition)\n", name);
+	return 2;
+}
+
 int main(int argc, char* argv[]) {
 	int partNum;
 	AbstractFile* in;
 	AbstractFile* out;
-	int hasKey;
+	int opt;
+	char *cmd, *infile, *outfile;
+	char *key = NULL;
 	
 	TestByteOrder();
-	
-	if(argc < 4) {
-		printf("usage: %s [extract|build|build2048|res|iso|dmg|attribute] <in> <out> (-k <key>) (partition)\n", argv[0]);
-		return 0;
-	}
 
-	if(!buildInOut(argv[2], argv[3], &in, &out)) {
-		return FALSE;
-	}
-
-	hasKey = FALSE;
-	if(argc > 5) {
-		if(strcmp(argv[4], "-k") == 0) {
-			in = createAbstractFileFromFileVault(in, argv[5]);
-			hasKey = TRUE;
+	while ((opt = getopt(argc, argv, "k:")) != -1) {
+		switch (opt) {
+			case 'k':
+				key = optarg;
+				break;
+			default:
+				return usage(argv[0]);
 		}
 	}
-	
 
-	if(strcmp(argv[1], "extract") == 0) {
+	if (argc < optind + 3) {
+		return usage(argv[0]);
+	}
+	cmd = argv[optind++];
+	infile = argv[optind++];
+	outfile = argv[optind++];
+
+	if(!buildInOut(infile, outfile, &in, &out)) {
+		return -1;
+	}
+	if(key != NULL) {
+		in = createAbstractFileFromFileVault(in, key);
+	}
+
+	if(strcmp(cmd, "extract") == 0) {
 		partNum = -1;
-		
-		if(hasKey) {
-			if(argc > 6) {
-				sscanf(argv[6], "%d", &partNum);
-			}
-		} else {
-			if(argc > 4) {
-				sscanf(argv[4], "%d", &partNum);
-			}
+		if (optind < argc) {
+				sscanf(argv[optind++], "%d", &partNum);
 		}
 		extractDmg(in, out, partNum);
-	} else if(strcmp(argv[1], "build") == 0) {
-		buildDmg(in, out, SECTOR_SIZE, argc > 4 ? argv[4] : NULL);
-	} else if(strcmp(argv[1], "build2048") == 0) {
-		buildDmg(in, out, 2048, NULL);
-	} else if(strcmp(argv[1], "res") == 0) {
-		outResources(in, out);
-	} else if(strcmp(argv[1], "iso") == 0) {
-		convertToISO(in, out);
-	} else if(strcmp(argv[1], "dmg") == 0) {
-		convertToDMG(in, out);
-	} else if(strcmp(argv[1], "attribute") == 0) {
-		if(argc < 6) {
-			printf("Not enough arguments: attribute <in> <out> <sentinel> <string>");
+	} else if(strcmp(cmd, "build") == 0) {
+		char *anchor = NULL;
+		if (argc >= optind) {
+			anchor = argv[optind++];
 		}
-		updateAttribution(in, out, argv[4], argv[5], strlen(argv[5]));
+		buildDmg(in, out, SECTOR_SIZE, anchor);
+	} else if(strcmp(cmd, "build2048") == 0) {
+		buildDmg(in, out, 2048, NULL);
+	} else if(strcmp(cmd, "res") == 0) {
+		outResources(in, out);
+	} else if(strcmp(cmd, "iso") == 0) {
+		convertToISO(in, out);
+	} else if(strcmp(cmd, "dmg") == 0) {
+		convertToDMG(in, out);
+	} else if(strcmp(cmd, "attribute") == 0) {
+		char *anchor, *data;
+		if(argc < optind + 2) {
+			printf("Not enough arguments: attribute <in> <out> <sentinel> <string>");
+			return 2;
+		}
+		anchor = argv[optind++];
+		data = argv[optind++];
+		updateAttribution(in, out, anchor, data, strlen(data));
 	}
 
 	return 0;
